25b4cfcd5d94a4cfbc126fcd9c5a4a4955b1c07e
PolishCivil <polishcivil@tutanota.com>
2025-05-31 02:20:03 +0200

    major: 117hd 230

--- 117hd/src/main/java/rs117/hd/overlays/TileInfoOverlay.java
+++ 117hd/src/main/java/rs117/hd/overlays/TileInfoOverlay.java
@@ -460,16 +460,11 @@ public class TileInfoOverlay extends Overlay implements MouseListener, MouseWhee
 			if (tile.getBridge() != null)
 				lines.add("Bridge");
 
-			var regionID = HDUtils.worldToRegionID(worldPos);
-			var regionX = regionID >> 8;
-			var regionY = regionID & 0xff;
 			lines.add("Scene point: " + tileX + ", " + tileY + ", " + tileZ);
 			lines.add("World point: " + Arrays.toString(worldPos));
 			lines.add(String.format(
-				"Region ID: %d [%d_%d] (%d, %d)",
+				"Region ID: %d (%d, %d)",
 				HDUtils.worldToRegionID(worldPos),
-				regionX,
-				regionY,
 				worldPos[0] >> 6,
 				worldPos[1] >> 6
 			));
